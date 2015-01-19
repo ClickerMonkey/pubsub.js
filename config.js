e6c39bb5031049eba2109395c3b9cd4119018a34
Philip Diffenderfer <pdiffenderfer@gmail.com>
2015-01-19 15:54:46 -0500

    Replace tabs with spaces

--- config.js
+++ config.js
@@ -1,119 +1,119 @@
 
 module.exports = 
 {
-	/**
-	 * The port the application listens on.
-	 */
-	port: 3000,
-	
-	/**
-	 * Whether or not to send a publish message back to the client who sent it.
-	 */
-	echoPublish: false,
-	
-	/**
-	 * If this server should notify clients when other clients have joined or left
-	 * a channel. The client's token it used to subscribe is sent along with these
-	 * notifications so the client can identify them. If this is set to false
-	 * then tokens are ignored altogether in the system. TODO
-	 */
-	sendJoinLeaveEvents: true,
-	
-	/**
-	 * The number of previous publishes to keep and send to a client when they join.
-	 */
-	sendLastPublishesOnJoin: 10,
-	
-	/**
-	 * Whether or not to send all of the join tokens of current clients to the 
-	 * new client when they first join the channel.
-	 */
-	sendExistingClientsOnJoin: true,
-	
-	/**
-	 * Requires that a client must be subscribed to a channel before they can publish in it.
-	 */
-	requireSubscription: true,
-	
-	/**
-	 * Logs events when true.
-	 */
-	debug: true,
-	
-	/****************************************************************************
-	 *                C H A N N E L    I D    V A L I D A T I O N
-	 ****************************************************************************/
-	
-	/**
-	 * The data types that are valid channel IDs.
-	 * If the id is found not to be valid, a channel will not be created.
-	 */
-	validIds: 
-	{
-		'number': 		true,
-		'string': 		true,
-		'boolean': 		true,
-		'object': 		false,
-		'undefined': 	false
-	},
-	
-	/**
-	 * A function which does further validation on a channel ID.
-	 * If the id is found not to be valid, a channel will not be created.
-	 */
-	validateId: function(id)
-	{
-		return true;
-	},
-	
-	/****************************************************************************
-	 *             C L I E N T    T O K E N    V A L I D A T I O N
-	 ****************************************************************************/
-	
-	/**
-	 * The data types that are valid client tokens.
-	 * If a token is found to be not valid, the user does not join the channel.
-	 */
-	validTokens:
-	{
-		'number': 		true,
-		'string': 		true,
-		'boolean': 		true,
-		'undefined': 	true,
-		'object': 		false
-	},
-	
-	/**
-	 * A function which does further validation on a client token.
-	 * If a token is found to be not valid, the user does not join the channel.	
-	 */
-	validateToken: function(token)
-	{
-		return true;
-	},
-	
-	/****************************************************************************
-	 *                 P U B L I S H    V A L I D A T I O N
-	 ****************************************************************************/
-	
-	/**
-	 * The data types that are valid to publish to other clients.
-	 */
-	validPublish:
-	{
-		'number': 		true,
-		'string': 		true,
-		'boolean': 		true,
-		'undefined': 	true,
-		'object': 		true
-	},
-	
-	/**
-	 * A function which validates if a message by a client can be published on a channel.
-	 */
-	validatePublish: function(message, client, channel)
-	{
-		return true;
-	}
-	
+  /**
+   * The port the application listens on.
+   */
+  port: 3000,
+  
+  /**
+   * Whether or not to send a publish message back to the client who sent it.
+   */
+  echoPublish: false,
+  
+  /**
+   * If this server should notify clients when other clients have joined or left
+   * a channel. The client's token it used to subscribe is sent along with these
+   * notifications so the client can identify them. If this is set to false
+   * then tokens are ignored altogether in the system. TODO
+   */
+  sendJoinLeaveEvents: true,
+  
+  /**
+   * The number of previous publishes to keep and send to a client when they join.
+   */
+  sendLastPublishesOnJoin: 10,
+  
+  /**
+   * Whether or not to send all of the join tokens of current clients to the 
+   * new client when they first join the channel.
+   */
+  sendExistingClientsOnJoin: true,
+  
+  /**
+   * Requires that a client must be subscribed to a channel before they can publish in it.
+   */
+  requireSubscription: true,
+  
+  /**
+   * Logs events when true.
+   */
+  debug: true,
+  
+  /****************************************************************************
+   *                C H A N N E L    I D    V A L I D A T I O N
+   ****************************************************************************/
+  
+  /**
+   * The data types that are valid channel IDs.
+   * If the id is found not to be valid, a channel will not be created.
+   */
+  validIds: 
+  {
+    'number':     true,
+    'string':     true,
+    'boolean':     true,
+    'object':     false,
+    'undefined':   false
+  },
+  
+  /**
+   * A function which does further validation on a channel ID.
+   * If the id is found not to be valid, a channel will not be created.
+   */
+  validateId: function(id)
+  {
+    return true;
+  },
+  
+  /****************************************************************************
+   *             C L I E N T    T O K E N    V A L I D A T I O N
+   ****************************************************************************/
+  
+  /**
+   * The data types that are valid client tokens.
+   * If a token is found to be not valid, the user does not join the channel.
+   */
+  validTokens:
+  {
+    'number':     true,
+    'string':     true,
+    'boolean':     true,
+    'undefined':   true,
+    'object':     false
+  },
+  
+  /**
+   * A function which does further validation on a client token.
+   * If a token is found to be not valid, the user does not join the channel.  
+   */
+  validateToken: function(token)
+  {
+    return true;
+  },
+  
+  /****************************************************************************
+   *                 P U B L I S H    V A L I D A T I O N
+   ****************************************************************************/
+  
+  /**
+   * The data types that are valid to publish to other clients.
+   */
+  validPublish:
+  {
+    'number':     true,
+    'string':     true,
+    'boolean':     true,
+    'undefined':   true,
+    'object':     true
+  },
+  
+  /**
+   * A function which validates if a message by a client can be published on a channel.
+   */
+  validatePublish: function(message, client, channel)
+  {
+    return true;
+  }
+  
 };
--- pubsub-server.js
+++ pubsub-server.js
@@ -1,11 +1,11 @@
-var app 		= require('express')();
-var http 		= require('http').Server(app);
-var io 			= require('socket.io')(http);
+var app     = require('express')();
+var http     = require('http').Server(app);
+var io       = require('socket.io')(http);
 
-var config 	= require('./config');
-var IdMap 	= require('./lib/IdMap');
+var config   = require('./config');
+var IdMap   = require('./lib/IdMap');
 var Channel = require('./lib/Channel');
-var Client	= require('./lib/Client');
+var Client  = require('./lib/Client');
 
 global.$channels = new IdMap();
 
@@ -14,127 +14,127 @@ global.$channels = new IdMap();
  */
 io.on('connection', function(socket)
 {
-	var client = new Client( socket );
-	
-	if (config.debug)
-	{
-		console.log( 'Client Connected', socket.id );
-	}
+  var client = new Client( socket );
   
-	/**
-	 * Handle when the client tries to subscribe to a channel.
-	 */
+  if (config.debug)
+  {
+    console.log( 'Client Connected', socket.id );
+  }
+  
+  /**
+   * Handle when the client tries to subscribe to a channel.
+   */
   socket.on('subscribe', function(msg) 
   {
-		var token = msg.token;
-		var channelId = msg.id;
-		
-		if (config.validTokens[ typeof token ] && config.validateToken( token )) 
-		{
-	    var channel = Channel.get( channelId, true );
-			
-			if (channel) 
-			{
-				client.join( channel, token );
-				
-				if (config.debug)
-				{
-					console.log( 'Client', socket.id, 'subscribed to channel', channelId, 'with token', token );
-				}
-			}
-		}
-		else if (config.debug)
-		{
-			console.log( 'Client', socket.id, 'sent invalid token', token );
-		}
+    var token = msg.token;
+    var channelId = msg.id;
+    
+    if (config.validTokens[ typeof token ] && config.validateToken( token )) 
+    {
+      var channel = Channel.get( channelId, true );
+      
+      if (channel) 
+      {
+        client.join( channel, token );
+        
+        if (config.debug)
+        {
+          console.log( 'Client', socket.id, 'subscribed to channel', channelId, 'with token', token );
+        }
+      }
+    }
+    else if (config.debug)
+    {
+      console.log( 'Client', socket.id, 'sent invalid token', token );
+    }
   });
   
-	/**
-	 * Handle when the client tries to unsubscribe from a channel.
-	 */
+  /**
+   * Handle when the client tries to unsubscribe from a channel.
+   */
   socket.on('unsubscribe', function(msg) 
   {
-		var channelId = msg.id;
-		var channel = Channel.get( channelId, false );
-		
-		var channelValid = !!channel;
-		
-		if (channel)
-		{
-			if (channel.has( client ) && client.subscribed( channel ))
-			{
-				client.leave( channel );
-			
-				if (config.debug)
-				{
-					console.log( 'Client', socket.id, 'unsubscribed from channel', channelId );
-				}
-			}
-			else if (config.debug)
-			{
-				console.log( 'Client', socket.id, 'does not participate in the channel', channelId, 'so they cannot unsubscribe' );
-			}
-		}
-		else if (config.debug)
-		{
-			console.log( 'Client', socket.id, 'cannot unsubscribe from the channel', channelId, ', it does not exist' );
-		}
+    var channelId = msg.id;
+    var channel = Channel.get( channelId, false );
+    
+    var channelValid = !!channel;
+    
+    if (channel)
+    {
+      if (channel.has( client ) && client.subscribed( channel ))
+      {
+        client.leave( channel );
+      
+        if (config.debug)
+        {
+          console.log( 'Client', socket.id, 'unsubscribed from channel', channelId );
+        }
+      }
+      else if (config.debug)
+      {
+        console.log( 'Client', socket.id, 'does not participate in the channel', channelId, 'so they cannot unsubscribe' );
+      }
+    }
+    else if (config.debug)
+    {
+      console.log( 'Client', socket.id, 'cannot unsubscribe from the channel', channelId, ', it does not exist' );
+    }
   });
   
-	/**
-	 * Handle when the client tries to publish to a channel.
-	 */
+  /**
+   * Handle when the client tries to publish to a channel.
+   */
   socket.on('publish', function(msg) 
   {
-		var data = msg.data;
-		var channelId = msg.id;
-		var channel = Channel.get( channelId, false );
-		
-		if (channel)
-		{
-			// if you need to be subscribed to publish...
-			if (!config.requireSubscription || client.subscribed( channel ))
-			{
-				// if it's a valid publish
-				if (config.validPublish[typeof data] && config.validatePublish( data, client, channel ))
-				{
-					channel.publish( client, {
-						id: channelId,
-						data: data
-					});
-					
-					if (config.debug)
-					{
-						console.log( 'Client', socket.id, 'published to channel', channelId, ':', data );
-					}
-				}
-				else if (config.debug)
-				{
-					console.log( 'Client', socket.id, 'sent an invalid publish to channel', channelId, ':', data );
-				}
-			}
-			else if (config.debug)
-			{
-				console.log( 'Client', socket.id, 'tried to publish to channel', channelId, 'but is not subscribed' );
-			}
-		}
-		else if (config.debug)
-		{
-			console.log( 'Client', socket.id, 'tried to publish to channel', channelId, 'which does not exist' );
-		}
+    var data = msg.data;
+    var channelId = msg.id;
+    var channel = Channel.get( channelId, false );
+    
+    if (channel)
+    {
+      // if you need to be subscribed to publish...
+      if (!config.requireSubscription || client.subscribed( channel ))
+      {
+        // if it's a valid publish
+        if (config.validPublish[typeof data] && config.validatePublish( data, client, channel ))
+        {
+          channel.publish( client, {
+            id: channelId,
+            data: data
+          });
+          
+          if (config.debug)
+          {
+            console.log( 'Client', socket.id, 'published to channel', channelId, ':', data );
+          }
+        }
+        else if (config.debug)
+        {
+          console.log( 'Client', socket.id, 'sent an invalid publish to channel', channelId, ':', data );
+        }
+      }
+      else if (config.debug)
+      {
+        console.log( 'Client', socket.id, 'tried to publish to channel', channelId, 'but is not subscribed' );
+      }
+    }
+    else if (config.debug)
+    {
+      console.log( 'Client', socket.id, 'tried to publish to channel', channelId, 'which does not exist' );
+    }
   });
   
-	/**
-	 * Handle when a client disconnects from the server.
-	 */
+  /**
+   * Handle when a client disconnects from the server.
+   */
   socket.on('disconnect', function() 
   {
-		client.destroy();
-		
-		if (config.debug)
-		{
-			console.log( 'Client Disconnected', socket.id );
-		}
+    client.destroy();
+    
+    if (config.debug)
+    {
+      console.log( 'Client Disconnected', socket.id );
+    }
   });  
 });
 
